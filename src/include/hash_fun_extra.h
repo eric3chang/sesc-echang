cf3f0359629dfe019fb9ba0f01ad8d52ad84f2fa
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-06-04 10:52:29 +0000

    added -Wall to compiler options. Fixed some warnings. Removed unnecessary namespace macro comments in hash_fun_extra.h.

--- src/include/hash_fun_extra.h
+++ src/include/hash_fun_extra.h
@@ -10,12 +10,7 @@
 
 #include <cstddef>
 
-//using namespace __gnu_cxx;
-
-//_GLIBCXX_BEGIN_NAMESPACE(__gnu_cxx)
-
 using std::size_t;
-using __gnu_cxx::hash;
 
 namespace __gnu_cxx
 {
@@ -25,7 +20,5 @@ namespace __gnu_cxx
     	{ return __x; }
    };
 }
-
-//_GLIBCXX_END_NAMESPACE
 
 #endif /* HASH_FUN_EXTRA_H_ */
