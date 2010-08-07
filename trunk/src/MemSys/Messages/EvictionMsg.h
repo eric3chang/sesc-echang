ee0e19919c939c5007c9f2843074be426791fc59
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-08-07 03:46:58 +0000

    removed some code that didn't need to be there

--- trunk/src/MemSys/Messages/EvictionMsg.h
+++ trunk/src/MemSys/Messages/EvictionMsg.h
@@ -12,11 +12,6 @@ namespace Memory
 
 		virtual bool IsResponse() const { return false; }
 		virtual size_t MsgSize() const { return 1 + sizeof(Address) + (blockAttached?size:0); }
-		// TODO: Eric 2010/08/04
-		virtual MessageID Msgid() const {
-		   return 0;
-		}
-
 		virtual MsgType Type() const { return mt_Eviction; }
 	};
 }
