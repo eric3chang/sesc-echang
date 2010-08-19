5011ec49f5746335b962eed06feab3d755095927
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-08-19 04:40:21 +0000

    made it so that we can compile sesc programs with synchronization, but it doesn't seem to work well.

--- src/libapp/memtest.c
+++ src/libapp/memtest.c
@@ -1,7 +1,12 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 //#include <pthread.h>
 #include "sescapi.h"
 #include <stdio.h>
-#define NUM_THREADS        2
+#include <stdlib.h>
+#define NUM_THREADS        5000
 
 #define SIM_CHECKPOINT 2
 #define SIM_START_RABBIT 0
@@ -20,9 +25,8 @@
 //unsigned char myCharArray [2];
 //unsigned char myChar = 0;
 //int counter = 0;
-#define SIZE 2
-char *myCharArray;
-int counter = -1;
+//char *myCharArray;
+//int counter = -1;
 /*
 char myString0[8] = "0000000";
 char myString1[8] = "1111111";
@@ -35,36 +39,48 @@ char myString7[8] = "7777777";
 char myString8[8] = "8888888";
 char myString9[8] = "9999999";
 */
+// ends rabbit mode
 
-void *readChar(ssema_t *sema)
+//void *readChar(ssema_t *sema)
+void *readChar(int *myIntArray)
+//void *readChar(int **args)
 {
-   int anotherCounter;
-   sesc_psema(sema);
-//	printf("myCharArray[%d]=%d\n", threadid, myCharArray[(int)threadid]);
+//   sesc_psema(sema);
+	printf("myIntArray[%d]=%d\n", myIntArray[0], myIntArray[myIntArray[0]]);
 //	printf("myCharArray[%d]=%d\n", counter, myCharArray[(int)counter]);
 //    pthread_exit(NULL);
 //   counter = counter + 1;
 //   printf("counter=%d\n", counter);
 
 //   anotherCounter = counter;
-   sesc_vsema(sema);
+//   sesc_vsema(sema);
 	sesc_exit(0);
 }
 
 int main()
 {
 //    pthread_t threads[NUM_THREADS];
-   char myChar;
+   int *myIntArray;
+   int myInt;
 	int t,i;
    ssema_t *sema;
+//   int **args;
+
+   // number of arguments is 2
+//   args = (int**)malloc(sizeof(int*) * 2);
+
+//   args[1] = myIntArray;
 
    // initialize array
-   myCharArray = malloc(sizeof(char) * SIZE);
-   for (i=0; i<SIZE; i++)
+
+   //TM_SIMOP(SIM_STOP_RABBIT);
+
+   myIntArray = (int*)malloc(sizeof(int) * NUM_THREADS);
+   for (i=0; i<NUM_THREADS; i++)
    {
-     myCharArray[i] = i % 128;
+     myIntArray[i] = i;
    }
-   sema = malloc(sizeof(ssema_t));
+   sema = (ssema_t*)malloc(sizeof(ssema_t));
 
    sesc_sema_init(sema, 1);
 
@@ -72,15 +88,23 @@ int main()
    for(t=0;t<NUM_THREADS;t++){
        printf("Creating thread %d\n", t);
 //       pthread_create(&threads[t], NULL, print_hello_world, (void *)t);
-	    sesc_spawn((void*) *readChar, sema, 0);
+//	    sesc_spawn((void (*)(void*)) *readChar, (void*) 0, 0);
+       myIntArray[0] = t;
+	    sesc_spawn((void (*)(void*)) *readChar, myIntArray, 0);
+//       *args[0] = t;
+//	    sesc_spawn((void (*)(void*)) *readChar, args, 0);
+//	    sesc_spawn((void (*)(void*)) *readChar, sema, 0);
     }
-//   for (i=0; i<1024*2; i++);
-//    pthread_exit(NULL);
    // wait for thread to finish
    sesc_wait();
 
-   free(myCharArray);
+//   free(myCharArray);
 
+//    pthread_exit(NULL);
 	sesc_exit(0);
 }
+
+#ifdef __cplusplus
+}
+#endif
 
