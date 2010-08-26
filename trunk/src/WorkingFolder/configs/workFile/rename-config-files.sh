c71812512fc6621c1d89c85e16dbfa6adc30fcb6
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-08-26 23:04:21 +0000

    checked in yada-dots-2cpu config files and checkpoint. Updated config generation scripts

--- trunk/src/WorkingFolder/configs/workFile/rename-config-files.sh
+++ trunk/src/WorkingFolder/configs/workFile/rename-config-files.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 # renaming underscored files to hyphen files
 
-for file in $(ls vacation_high__64_1_8_2_Perfect.conf_*); do
+for file in $(ls yada__64_1_8_2_Perfect.conf_*); do
    string=${file:0-2}
    # if length of string is 1, append 0 in front of it
    if [[ ${string:0:1} == _ ]]
    then
       string=0${string:1:1}
    fi
-   cp $file "vacation-high-02cpu-$string".conf
+   cp $file "yada-dots-02cpu-$string".conf
 done
 
