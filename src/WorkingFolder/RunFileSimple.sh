549ef459d77bfab452c19a5ee258f5cd0cdb1592
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-08-26 22:07:31 +0000

    added vacation low console outputs. Added vacation high checkpoint and config files. Modified generating config files

--- src/WorkingFolder/RunFileSimple.sh
+++ src/WorkingFolder/RunFileSimple.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-./augSesc -cconfigs/workFile/memtest-directory-02cpu.conf -dconfigs/workFile/memtest-directory-02cpu.report ../libapp/memtest.sesc
+./augSesc -w1 -cconfigs/workFile/newfile.conf -dconfigs/workFile/temp.report benchmarks/vacation -c2 -n2 -q90 -u98 -r16384 -t4096
+#./augSesc -cconfigs/workFile/memtest-directory-02cpu.conf -dconfigs/workFile/memtest-directory-02cpu.report ../libapp/memtest.sesc
 #-w1 -cconfigs/workFile/genome-snoopy.conf -dconfigs/workFile/genome-snoopy.report benchmarks/genome -t2 -g256 -s16 -n16384
 #./augSesc -w1 -cconfigs/workFile/genome__64_1_8_2_Perfect.conf_0 -dconfigs/workFile/genome__64_1_8_2_Perfect.conf_0.report ../libapp/hello.sesc -t2 -g256 -s16 -n16384
 #./augSesc -w1 -cconfigs/workFile/genome__64_1_8_2_Perfect.conf_0 -dconfigs/workFile/genome__64_1_8_2_Perfect.conf_0.report ~/csl/sesc-cvs/tests/mcf < ~/csl/sesc-cvs/tests/mcf.in -t2 -g256 -s16 -n16384 
