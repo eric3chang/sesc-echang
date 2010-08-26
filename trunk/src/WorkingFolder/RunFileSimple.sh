5122aa6652df2e9988a78db19b36068f744407f0
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-08-26 10:26:50 +0000

    included soft links to config files for working in linux

--- trunk/src/WorkingFolder/RunFileSimple.sh
+++ trunk/src/WorkingFolder/RunFileSimple.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
--w1 -cconfigs/workFile/genome-snoopy.conf -dconfigs/workFile/genome-snoopy.report benchmarks/genome -t2 -g256 -s16 -n16384
+./augSesc -cconfigs/workFile/memtest-directory-02cpu.conf -dconfigs/workFile/memtest-directory-02cpu.report ../libapp/memtest.sesc
+#-w1 -cconfigs/workFile/genome-snoopy.conf -dconfigs/workFile/genome-snoopy.report benchmarks/genome -t2 -g256 -s16 -n16384
 #./augSesc -w1 -cconfigs/workFile/genome__64_1_8_2_Perfect.conf_0 -dconfigs/workFile/genome__64_1_8_2_Perfect.conf_0.report ../libapp/hello.sesc -t2 -g256 -s16 -n16384
 #./augSesc -w1 -cconfigs/workFile/genome__64_1_8_2_Perfect.conf_0 -dconfigs/workFile/genome__64_1_8_2_Perfect.conf_0.report ~/csl/sesc-cvs/tests/mcf < ~/csl/sesc-cvs/tests/mcf.in -t2 -g256 -s16 -n16384 
