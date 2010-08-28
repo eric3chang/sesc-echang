0de512a36f5efe265bcfb8a77df55ad26fa6ad2c
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-08-28 00:47:49 +0000

    deleted old fft config files.

--- src/WorkingFolder/configs/workFile/rename-config-files.sh
+++ src/WorkingFolder/configs/workFile/rename-config-files.sh
@@ -1,20 +1,21 @@
 #/bin/bash
 # rename files to create more config files
 
+#set -x   # verbose output
+
 #for file in $(ls yada__64_1_8_2_Perfect.conf_*); do
-   string=${file:0-2}
+#   string=${file:0-2}
    # if length of string is 1, append 0 in front of it
    #if [[ ${string:0:1} == _ ]]
 #   then
-      string=0${string:1:1}
+#      string=0${string:1:1}
 #   fi
 #   cp $file "fft-02cpu-$string".conf
 #done
 
-for file in $(ls genome-02cpu-??.conf); do
+for file in $(ls fft-02cpu-??.conf); do
    string=${file:0-7:2}
-   cp $file "fft-02cpu-$string".conf
-#   echo $file "fft-02cpu-$string".conf
+   cp $file "cholesky-02cpu-$string".conf
 done
 
 
