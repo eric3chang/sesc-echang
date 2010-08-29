c6c114929f0f22bc6cd869a86fca9d75c60a32a8
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-08-29 05:29:50 +0000

    changed runfile-cholesky-* to runfile-cholesky-lshp-* . Added radix configs and runfile

--- trunk/src/WorkingFolder/configs/workFile/make-new-configs.sh
+++ trunk/src/WorkingFolder/configs/workFile/make-new-configs.sh
@@ -4,7 +4,7 @@
 #set -x   # verbose output
 OLD_BENCHMARK=cholesky # benchmark name of old config files
 OLD_SETTING=002-0001-0002  # processor and cache settings of old config files
-NEW_BENCHMARK=barnes   # benchmark name of new config files
+NEW_BENCHMARK=radix   # benchmark name of new config files
 NEW_SETTING=002-0001-0002  # processor and cache settings of new config files
 
 for file in $(ls $OLD_BENCHMARK-$OLD_SETTING-??.conf); do
