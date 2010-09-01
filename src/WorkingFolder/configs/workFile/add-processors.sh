d142b6d1234ec343202fbd5901695c746b1fc2f3
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-09-01 09:02:04 +0000

    Added fft 8 cpu files

--- src/WorkingFolder/configs/workFile/add-processors.sh
+++ src/WorkingFolder/configs/workFile/add-processors.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-for oldFile in $(ls genome__64_1_8_4_Perfect.conf_*)
+for oldFile in $(ls genome__64_1_8_8_Perfect.conf_*)
 do
    configIndex=${oldFile:0-2:2}
    if [[ ${configIndex:0:1} == _ ]]
    then
       configIndex=0${configIndex:1:1}
    fi
-   processorSkew2=$(cat $oldFile | grep 'ProcessorSkew\[2\]')
-   processorSkew3=$(cat $oldFile | grep 'ProcessorSkew\[3\]')
-   sed -i\
-      -e "11i$processorSkew2" \
-      -e "11i$processorSkew3" \
-      fft-004-0001-0002-$configIndex.conf
+   processorSkew4=$(cat $oldFile | grep 'ProcessorSkew\[4\]')
+   processorSkew5=$(cat $oldFile | grep 'ProcessorSkew\[5\]')
+   processorSkew6=$(cat $oldFile | grep 'ProcessorSkew\[6\]')
+   processorSkew7=$(cat $oldFile | grep 'ProcessorSkew\[7\]')
+   sed \
+      -e "11i$processorSkew4" \
+      -e "11i$processorSkew5" \
+      -e "11i$processorSkew6" \
+      -e "11i$processorSkew7" \
+      fft-008-0001-0002-$configIndex.conf
 done
