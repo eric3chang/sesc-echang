2bc043ad8ddc2a2caa88aafb3a6ea69ded0e8eab
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-09-14 00:51:46 +0000

    added EvictionBusyAckMsg

---
+++ trunk/src/MemSys/Messages/EvictionBusyAckMsg.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "BaseMsg.h"
+#include "../MSTypes.h"
+
+namespace Memory
+{
+	class EvictionBusyAckMsg : public BaseMsg
+	{
+	public:
+		Address addr;
+      bool isExclusive;
+		size_t size;
+		MessageID solicitingMessage;
+
+		virtual bool IsResponse() const { return true; }
+		virtual size_t MsgSize() const { return 1 + sizeof(Address); }
+		virtual MsgType Type() const { return mt_EvictionBusyAck; }
+   protected:
+      virtual void print(DeviceID destinationDeviceID) const
+      {
+         BaseMsg::print(destinationDeviceID);
+         cout << " addr=" << addr
+            << " isExclusive=" << isExclusive
+            << " solicitMsg=" << solicitingMessage
+         ;
+      }
+	};
+}
