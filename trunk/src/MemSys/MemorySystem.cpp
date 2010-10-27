79b09a5f15bfe4fdbfeceb98dc44827454ce6ff0
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2010-10-27 18:06:47 +0000

    checked in MSICache

--- trunk/src/MemSys/MemorySystem.cpp
+++ trunk/src/MemSys/MemorySystem.cpp
@@ -36,6 +36,7 @@ namespace Memory
 	   deviceFactory.RegisterNode("Directory",new Factory<std::string,BaseMemDevice>::GenericFactoryNode<Directory>());
 	   deviceFactory.RegisterNode("ThreeStageDirectory",new Factory<std::string,BaseMemDevice>::GenericFactoryNode<ThreeStageDirectory>());
 		deviceFactory.RegisterNode("MOESICache",new Factory<std::string,BaseMemDevice>::GenericFactoryNode<MOESICache>());
+      deviceFactory.RegisterNode("MSICache",new Factory<std::string,BaseMemDevice>::GenericFactoryNode<MSICache>());
 		deviceFactory.RegisterNode("NetworkMemoryInterface",new Factory<std::string,BaseMemDevice>::GenericFactoryNode<NetworkMemoryInterface>());
 		deviceFactory.RegisterNode("RandomLoadNetwork",new Factory<std::string,BaseMemDevice>::GenericFactoryNode<RandomLoadNetwork>());
 		deviceFactory.RegisterNode("SnoopyBus",new Factory<std::string,BaseMemDevice>::GenericFactoryNode<SnoopyBus>());
