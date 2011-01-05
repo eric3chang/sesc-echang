e873f1a191432162342824c8f972979145e40264
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-01-05 22:44:28 +0000

    fixed runfile-fft-snoopy-moesi-064-0016-1024.sh

--- src/WorkingFolder/runfile-fft-snoopy-moesi-064-0016-1024.sh
+++ src/WorkingFolder/runfile-fft-snoopy-moesi-064-0016-1024.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
 
-nice -10 ./augSesc-release  -cconfigs/workFile/fft-snoopy-064-0016-1024.conf -dconfigs/workFile/fft-snoopy-064-0016-1024.conf.report benchmarks-splash2-sesc/fft.mips -m12 -p64 -n65536 -l4 -t &> console-outputs/fft-snoopy-064-0016-1024.out.$HOSTNAME
+nice -10 ./augSesc-release  -cconfigs/workFile/fft-snoopy-moesi-064-0016-1024.conf -dconfigs/workFile/fft-snoopy-moesi-064-0016-1024.conf.report benchmarks-splash2-sesc/fft.mips -m12 -p64 -n65536 -l4 -t &> console-outputs/fft-snoopy-moesi-064-0016-1024.out.$HOSTNAME
