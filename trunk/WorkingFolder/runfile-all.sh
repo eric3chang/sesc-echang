8a92452501995a9b5c11aa5ecf542d2e6fe6b409
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-02-23 06:47:21 +0000

    changed directory to bip

--- trunk/WorkingFolder/runfile-all.sh
+++ trunk/WorkingFolder/runfile-all.sh
@@ -2,16 +2,16 @@
 HOSTNAME=$(hostname)
 
 ./runfile-barnes-origin-all.sh
-./runfile-barnes-directory-all.sh
+./runfile-barnes-bip-all.sh
 ./runfile-cholesky-origin-all.sh
-./runfile-cholesky-directory-all.sh
+./runfile-cholesky-bip-all.sh
 ./runfile-fft-origin-all.sh
-./runfile-fft-directory-all.sh
+./runfile-fft-bip-all.sh
 ./runfile-fmm-origin-all.sh
-./runfile-fmm-directory-all.sh
+./runfile-fmm-bip-all.sh
 ./runfile-radix-origin-all.sh
-./runfile-radix-directory-all.sh
+./runfile-radix-bip-all.sh
 ./runfile-raytrace-origin-all.sh
-./runfile-raytrace-directory-all.sh
+./runfile-raytrace-bip-all.sh
 ./runfile-ocean-origin-all.sh
-./runfile-ocean-directory-all.sh
+./runfile-ocean-bip-all.sh
