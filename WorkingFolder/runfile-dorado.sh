82c8223a85f805cf58aa9f9bb990487d72d950b7
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-02-23 21:45:33 +0000

    made modifications to runfiles

--- WorkingFolder/runfile-dorado.sh
+++ WorkingFolder/runfile-dorado.sh
@@ -3,5 +3,6 @@ HOSTNAME=$(hostname)
 
 ./runfile-barnes-origin-all.sh
 ./runfile-barnes-bip-all.sh
-./runfile-cholesky-origin-all.sh
-./runfile-cholesky-bip-all.sh
+# moved the following onto illiac
+#./runfile-cholesky-origin-all.sh
+#./runfile-cholesky-bip-all.sh
