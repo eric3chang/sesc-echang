a35b19dca3d17cdc1ebc5c60bd25b3c7068717e9
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-02-23 00:08:41 +0000

    checked in runfiles

---
+++ trunk/WorkingFolder/runfile-fft-origin-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+HOSTNAME=$(hostname)
+
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-2-16-1024.conf -dconfigs/workFile/fft-origin-2-16-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p2 -n65536 -l4 -t &> console-outputs/fft-origin-2-16-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-2-32-1024.conf -dconfigs/workFile/fft-origin-2-32-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p2 -n65536 -l4 -t &> console-outputs/fft-origin-2-32-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-2-64-1024.conf -dconfigs/workFile/fft-origin-2-64-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p2 -n65536 -l4 -t &> console-outputs/fft-origin-2-64-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-2-128-1024.conf -dconfigs/workFile/fft-origin-2-128-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p2 -n65536 -l4 -t &> console-outputs/fft-origin-2-128-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-4-16-1024.conf -dconfigs/workFile/fft-origin-4-16-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p4 -n65536 -l4 -t &> console-outputs/fft-origin-4-16-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-4-32-1024.conf -dconfigs/workFile/fft-origin-4-32-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p4 -n65536 -l4 -t &> console-outputs/fft-origin-4-32-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-4-64-1024.conf -dconfigs/workFile/fft-origin-4-64-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p4 -n65536 -l4 -t &> console-outputs/fft-origin-4-64-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-4-128-1024.conf -dconfigs/workFile/fft-origin-4-128-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p4 -n65536 -l4 -t &> console-outputs/fft-origin-4-128-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-8-16-1024.conf -dconfigs/workFile/fft-origin-8-16-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p8 -n65536 -l4 -t &> console-outputs/fft-origin-8-16-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-8-32-1024.conf -dconfigs/workFile/fft-origin-8-32-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p8 -n65536 -l4 -t &> console-outputs/fft-origin-8-32-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-8-64-1024.conf -dconfigs/workFile/fft-origin-8-64-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p8 -n65536 -l4 -t &> console-outputs/fft-origin-8-64-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-8-128-1024.conf -dconfigs/workFile/fft-origin-8-128-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p8 -n65536 -l4 -t &> console-outputs/fft-origin-8-128-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-16-16-1024.conf -dconfigs/workFile/fft-origin-16-16-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p16 -n65536 -l4 -t &> console-outputs/fft-origin-16-16-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-16-32-1024.conf -dconfigs/workFile/fft-origin-16-32-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p16 -n65536 -l4 -t &> console-outputs/fft-origin-16-32-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-16-64-1024.conf -dconfigs/workFile/fft-origin-16-64-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p16 -n65536 -l4 -t &> console-outputs/fft-origin-16-64-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-16-128-1024.conf -dconfigs/workFile/fft-origin-16-128-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p16 -n65536 -l4 -t &> console-outputs/fft-origin-16-128-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-32-16-1024.conf -dconfigs/workFile/fft-origin-32-16-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p32 -n65536 -l4 -t &> console-outputs/fft-origin-32-16-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-32-32-1024.conf -dconfigs/workFile/fft-origin-32-32-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p32 -n65536 -l4 -t &> console-outputs/fft-origin-32-32-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-32-64-1024.conf -dconfigs/workFile/fft-origin-32-64-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p32 -n65536 -l4 -t &> console-outputs/fft-origin-32-64-1024.out.$HOSTNAME
+nice -10 ./augSesc-Debug -cconfigs/workFile/fft-origin-32-128-1024.conf -dconfigs/workFile/fft-origin-32-128-1024.conf.report benchmarks-splash2-sesc/fft.mips -m10 -p32 -n65536 -l4 -t &> console-outputs/fft-origin-32-128-1024.out.$HOSTNAME
