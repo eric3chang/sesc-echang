a07bd46f314521cea513ad9a8b4616b7a7dce9f8
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-01 00:13:11 +0000

    deleted old runfiles

--- WorkingFolder/runfile-fft-snoopy-moesi-064-0016-1024.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-HOSTNAME=$(hostname)
-
-nice -10 ./augSesc-debug  -cconfigs/workFile/fft-snoopy-moesi-064-0016-1024.conf -dconfigs/workFile/fft-snoopy-moesi-064-0016-1024.conf.report benchmarks-splash2-sesc/fft.mips -m12 -p64 -n65536 -l4 -t &> console-outputs/fft-snoopy-moesi-064-0016-1024.out.$HOSTNAME
