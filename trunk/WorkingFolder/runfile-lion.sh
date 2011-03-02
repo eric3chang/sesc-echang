dd82b80a40b84bf2586ad09ab190a2db8e288a94
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-02 11:07:44 +0000

    added runfile-fmm-directory-all.sh

--- trunk/WorkingFolder/runfile-lion.sh
+++ trunk/WorkingFolder/runfile-lion.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
 
-./runfile-raytrace-origin-all.sh
-./runfile-raytrace-bip-all.sh
-./runfile-ocean-origin-all.sh
-./runfile-ocean-bip-all.sh
+./runfile-fmm-directory-all.sh
+#./runfile-raytrace-bip-all.sh
+#./runfile-raytrace-origin-all.sh
+#./runfile-ocean-bip-all.sh
+#./runfile-ocean-origin-all.sh
