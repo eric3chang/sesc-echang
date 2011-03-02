c1b4d7e690593e2c2cefa960e7ff26261c1a7c74
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-02 23:01:50 +0000

    checked in runfile-dorado.sh

--- WorkingFolder/runfile-dorado.sh
+++ WorkingFolder/runfile-dorado.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
 
+./runfile-cholesky-special.sh
+#./runfile-barnes-origin-all.sh
 #./runfile-fmm-bip-all.sh
 #./runfile-fmm-origin-all.sh
 #./runfile-barnes-bip-all.sh
 #./runfile-barnes-origin-all.sh
-./runfile-radix-bip-all.sh
-./runfile-radix-origin-all.sh
+#./runfile-radix-bip-all.sh
+#./runfile-radix-origin-all.sh
