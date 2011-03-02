c208ee671bee9f624c5648a54b6e9c95650b7898
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-02 22:18:57 +0000

    Added radix results

--- WorkingFolder/runfile-illiac.sh
+++ WorkingFolder/runfile-illiac.sh
@@ -7,5 +7,6 @@ HOSTNAME=$(hostname)
 #./runfile-fmm-origin-all.sh
 #./runfile-radix-bip-all.sh
 #./runfile-radix-origin-all.sh
-./runfile-cholesky-bip-all.sh
-./runfile-cholesky-origin-all.sh
+#./runfile-cholesky-bip-all.sh
+./runfile-cholesky-directory-all.sh
+#./runfile-cholesky-origin-all.sh
