898a656553716b8ef3d54fb72cef990dd6308a9d
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-02 11:04:24 +0000

    checked in memDevResults

--- trunk/WorkingFolder/runfile-dorado.sh
+++ trunk/WorkingFolder/runfile-dorado.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
 
-./runfile-fmm-bip-all.sh
-./runfile-fmm-origin-all.sh
+#./runfile-fmm-bip-all.sh
+#./runfile-fmm-origin-all.sh
 #./runfile-barnes-bip-all.sh
 #./runfile-barnes-origin-all.sh
+./runfile-radix-bip-all.sh
+./runfile-radix-origin-all.sh
