35798d1321c4d0f6d868a16cebd978121ffd4de9
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-03 21:52:53 +0000

    added memDevResults

--- trunk/WorkingFolder/runfile-lion.sh
+++ trunk/WorkingFolder/runfile-lion.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
 
+./runfile-fft-bip-all.sh
+./runfile-fft-origin-all.sh
+./runfile-cholesky-bip-all.sh
+./runfile-cholesky-origin-all.sh
+#./runfile-fmm-bip-special.sh
 #./runfile-fmm-directory-all.sh
+#./runfile-fmm-origin-all.sh
+./runfile-radix-bip-all.sh
+./runfile-radix-origin-all.sh
 #./runfile-raytrace-bip-all.sh
 #./runfile-raytrace-origin-all.sh
-./runfile-ocean-bip-special.sh
-#./runfile-ocean-origin-special.sh
+./runfile-ocean-bip-all.sh
+./runfile-ocean-origin-all.sh
