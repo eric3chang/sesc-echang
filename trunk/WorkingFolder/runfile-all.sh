680f1cfa78547c710a0aa2dd4c382b97ba39374e
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-04 21:38:19 +0000

    removed debug verbose. Checked in new runfiles

--- trunk/WorkingFolder/runfile-all.sh
+++ trunk/WorkingFolder/runfile-all.sh
@@ -4,3 +4,9 @@ HOSTNAME=$(hostname)
 
 ./runfile-cholesky-bip-all.sh
 ./runfile-cholesky-origin-all.sh
+./runfile-fft-bip-all.sh
+./runfile-fft-origin-all.sh
+./runfile-radix-bip-all.sh
+./runfile-radix-origin-all.sh
+./runfile-ocean-bip-all.sh
+./runfile-ocean-origin-all.sh
