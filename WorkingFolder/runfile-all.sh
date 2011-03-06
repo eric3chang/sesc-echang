f3d826c8ec437d1eca651dfe553eca56d581572f
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-06 10:29:21 +0000

    checking in some memDevResults and lots of conf files for 1kb-4kb

--- WorkingFolder/runfile-all.sh
+++ WorkingFolder/runfile-all.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 DATE=$(date "+%m%d%H%M")
 HOSTNAME=$(hostname)
+AUGSESC=augSesc-Release
 
-./runfile-cholesky-bip-all.sh
-./runfile-cholesky-origin-all.sh
-./runfile-fft-bip-all.sh
-./runfile-fft-origin-all.sh
 ./runfile-radix-bip-all.sh
 ./runfile-radix-origin-all.sh
-./runfile-ocean-bip-all.sh
-./runfile-ocean-origin-all.sh
