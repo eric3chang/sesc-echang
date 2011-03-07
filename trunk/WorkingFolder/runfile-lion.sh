e2fd9bdeb6f28d2fc47886584c52e6056ee4f5d8
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-07 12:23:52 +0000

    checked in radix memDevResults

--- trunk/WorkingFolder/runfile-lion.sh
+++ trunk/WorkingFolder/runfile-lion.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
 
-./runfile-fft-bip-all.sh
-./runfile-fft-origin-all.sh
-./runfile-cholesky-bip-all.sh
-./runfile-cholesky-origin-all.sh
+#./runfile-fft-bip-all.sh
+#./runfile-fft-origin-all.sh
+#./runfile-cholesky-bip-all.sh
+#./runfile-cholesky-origin-all.sh
 #./runfile-fmm-bip-special.sh
 #./runfile-fmm-directory-all.sh
 #./runfile-fmm-origin-all.sh
@@ -12,5 +12,5 @@ HOSTNAME=$(hostname)
 ./runfile-radix-origin-all.sh
 #./runfile-raytrace-bip-all.sh
 #./runfile-raytrace-origin-all.sh
-./runfile-ocean-bip-all.sh
-./runfile-ocean-origin-all.sh
+#./runfile-ocean-bip-all.sh
+#./runfile-ocean-origin-all.sh
