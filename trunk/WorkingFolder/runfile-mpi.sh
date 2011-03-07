aa50b0b3ea2e7e598a24a2a2ff60a5e5cb068131
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-07 21:51:22 +0000

    checked in some memDevResults

--- trunk/WorkingFolder/runfile-mpi.sh
+++ trunk/WorkingFolder/runfile-mpi.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-./runfile-volrend-all.sh
+DATE=$(date "+%m%d%H%M")
+HOSTNAME=$(hostname)
+AUGSESC=augSesc-Debug
 
-# seg-faults, don't use
-#./runfile-water-nsquared-all.sh
-#./runfile-water-spatial-all.sh
-
+#./runfile-barnes-bip-all.sh
+#./runfile-barnes-origin-all.sh
+#./runfile-cholesky-bip-all.sh
+#./runfile-cholesky-origin-all.sh
+#./runfile-fft-bip-all.sh
+#./runfile-fft-origin-all.sh
+#./runfile-lu-bip-all.sh
+#./runfile-lu-origin-all.sh
+#./runfile-newtest-bip-all.sh
+#./runfile-newtest-origin-all.sh
+#./runfile-radix-bip-all.sh
+#./runfile-radix-origin-all.sh
+#./runfile-raytrace-bip-all.sh
+#./runfile-raytrace-origin-all.sh
+#./runfile-ocean-bip-all.sh
+./runfile-ocean-origin-special.sh
