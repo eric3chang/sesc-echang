fb26434e68a5ed9bc6fccaeb507d8c29f900a4e5
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-07 06:17:12 +0000

    checked in memDevResults for newtest

--- trunk/WorkingFolder/runfile-dorado.sh
+++ trunk/WorkingFolder/runfile-dorado.sh
@@ -4,8 +4,8 @@ HOSTNAME=$(hostname)
 
 #./runfile-cholesky-bip-all.sh
 #./runfile-cholesky-origin-all.sh
-#./runfile-fft-bip-all.sh
-#./runfile-fft-origin-all.sh
+./runfile-fft-bip-all.sh
+./runfile-fft-origin-all.sh
 #./runfile-radix-bip-all.sh
 #./runfile-radix-origin-all.sh
 #./runfile-ocean-bip-all.sh
