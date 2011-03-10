3ae9ceb23a1a0318081f5f67a54ccb7c52087515
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-10 22:35:08 +0000

    added lu-bip-memDevResults

--- trunk/WorkingFolder/runfile-lion1.sh
+++ trunk/WorkingFolder/runfile-lion1.sh
@@ -4,13 +4,14 @@
 #./runfile-cholesky-origin-all.sh
 #./runfile-fft-bip-all.sh
 #./runfile-fft-origin-all.sh
-./runfile-lu-bip-all.sh
-./runfile-lu-origin-all.sh
+#./runfile-lu-bip-all.sh
+#./runfile-lu-origin-all.sh
+./runfile-lu-simple.sh
 #./runfile-newtest-bip-all.sh
 #./runfile-newtest-origin-all.sh
 #./runfile-radix-bip-all.sh
 #./runfile-radix-origin-all.sh
-#./runfile-raytrace-bip-all.sh
+./runfile-raytrace-bip-all.sh
 #./runfile-raytrace-origin-all.sh
 #./runfile-ocean-bip-all.sh
 #./runfile-ocean-origin-all.sh
