762e3e7cfc2a19b9ecf479c4558250f7a376e8ca
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-11 21:45:06 +0000

    deleted radix confs

--- WorkingFolder/runfile-dorado.sh
+++ WorkingFolder/runfile-dorado.sh
@@ -2,7 +2,7 @@
 
 #./runfile-ocean-bip-all.sh
 #./runfile-ocean-origin-all.sh
-./runfile-lu-bip-all.sh
+#./runfile-lu-bip-all.sh
 #./runfile-lu-origin-all.sh
-#./runfile-cholesky-bip-all.sh
-#./runfile-cholesky-origin-all.sh
+./runfile-cholesky-bip-all.sh
+./runfile-cholesky-origin-all.sh
