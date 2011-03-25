80d3725c20b918ba7612636fdca64a949d441df6
eric3chang <eric3chang@8141ba7c-467b-0513-4b52-2e5cf4a2a9cd>
2011-03-25 16:56:44 +0000

    checked in new generate scripts

--- src/WorkingFolder/runfile-dorado.sh
+++ src/WorkingFolder/runfile-dorado.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DATE=$(date "+%m%d%H%M")
-HOSTNAME=$(hostname)
-AUGSESC=augSesc-Debug
 
-nice -10 ./$AUGSESC -cconfigs/workFile/fft-origin-32-64-128.conf -dconfigs/workFile/fft-origin-32-64-128.conf.report benchmarks-splash2-sesc/fft.mips -m12 -p32 -n65536 -l4 -t
+#./runfile-cholesky-bip-all.sh
+#./runfile-cholesky-origin-all.sh
+./runfile-ocean-bip-all.sh
+#./runfile-ocean-origin-all.sh
